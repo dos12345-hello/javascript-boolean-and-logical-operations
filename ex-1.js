a4801c5123fb8dcda68990d6920d5c4bc6ee5706
Khanatip Chaisri <doskhanatip@gmail.com>
2025-02-14 14:45:10 +0700

    boolean and logical operations assignment

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+let isAllow = true;
 
-let isAllow;
-
-// Start coding here
-
+isOver18 = false;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && hasCriminalBlacklist;
 console.log(isAllow);
